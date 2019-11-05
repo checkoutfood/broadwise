86c544a9c58497124b7f73787e78229240c67ea8
Chocks <checkoutfood.contact@gmail.com>
2019-11-05 13:49:51 +0000

    Initial commit

--- app.js
+++ app.js
@@ -30,9 +30,6 @@ const port = 3000;
 // CORS Middleware
 app.use(cors());
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Body Parser Middleware
 app.use(bodyParser.json());
 
@@ -44,14 +41,27 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
+
+// // Set Static Folder
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '/client/dist/client'))); // Provide static directory for frontend
+
+
 // Index Route
-app.get('/', (req, res) => {
-  res.send('Invalid Endpoint');
+// app.get('/', (req, res) => {
+//   //res.send('Invalid Endpoint');
+//   res.sendFile(path.join(__dirname,'public/index.html'));
+// });
+
+// app.get('*',(req,res)=>{
+//  res.sendFile(path.join(__dirname,'public/index.html'));
+// })
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
 
-app.get('*',(req,res)=>{
- res.sendFile(path.join(__dirname,'public/index.html'));
-})
 
 // Start Server
 app.listen(port, () => {
