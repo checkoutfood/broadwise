d59fb1570839e35b537a93bc9e3f11a0598ba11a
Chocks <checkoutfood.contact@gmail.com>
2019-11-05 13:55:44 +0000

    Initial commit

--- app.js
+++ app.js
@@ -57,6 +57,10 @@ app.use(express.static(path.join(__dirname + '/client/dist/client'))); // Provid
 //  res.sendFile(path.join(__dirname,'public/index.html'));
 // })
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+});
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
